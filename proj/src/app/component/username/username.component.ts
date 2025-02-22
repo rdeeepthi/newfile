cbe4b4a699506128a0b62c9763c0fdbebfdcd14a
rdeeepthi <rdeepthi231@gmail.com>
2025-02-22 13:04:51 +0530

    first commit

---
+++ proj/src/app/component/username/username.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  standalone: false,
+  templateUrl: './username.component.html',
+  styleUrl: './username.component.scss'
+})
+export class UsernameComponent {
+
+}
